07fd22df6d28edf0ed613063d092601f6f76543d
XtiaN <65335377+k1ch4n@users.noreply.github.com>
2021-08-26 05:10:35 -0500

    v_final
    Se añadió otro tipo de manómetro al dataset, ser verifico predicciones en codigo

--- Dataset_entrenamiento_test_bajo/de_video_foto.py
+++ Dataset_entrenamiento_test_bajo/de_video_foto.py
@@ -3,19 +3,19 @@ import cv2
 import os
  
  # Use VideoCapture para capturar video, aquí usamos video local
-cap = cv2.VideoCapture("bajo_1.mp4")
+cap = cv2.VideoCapture("man2_bajo.mp4")
  
  # Crea un archivo para guardar el fotograma del video
-save_path = os.makedirs("bajo")
+save_path = os.makedirs("bajo_man") #con makedirs, mkdir si existe lanza error, con exist_ok=true chancas
  
  # Nombra la captura de pantalla de la imagen
 imgPath = ""
  
  # Número total de fotogramas de video
-sum = cap.get(7)
+sum = cap.get(5)
  
  # De punto flotante a entero, calcula el intervalo de fotogramas interceptados, aquí interceptará 30 imágenes
-time = (int)(sum / 165)
+time = (int)(sum / 10)
  # Restablecer el número total de fotogramas de video
 sum = 0
  # El número de imágenes a interceptar
@@ -26,11 +26,11 @@ while True:
          # Leer fotograma de video
 	if ret == False:
 		break
-	sum += 1
+	sum += 1000
 	if sum % time == 0 and i < 5000:
 		i += 1
                  # Usa i para nombrar la imagen
-		imgPath = "bajo/bajo.%s.jpg" % str(i)
+		imgPath = "bajo_man/444%s.jpg" % str(i)
                  # Almacene el fotograma de video extraído en imgPath
 		cv2.imwrite(imgPath, frame)
  
